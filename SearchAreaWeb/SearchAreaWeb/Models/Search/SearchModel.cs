41c7e5fcd2466bb3596c750217fb78760987298c
Jbtyson <jbtyson@ksu.edu>
2015-07-18 14:36:11 -0500

    Init

---
+++ SearchAreaWeb/SearchAreaWeb/Models/Search/SearchModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchAreaWeb.Search.Models
+{
+    public class SearchModel
+    {
+        public SearchModel()
+        {
+
+        }
+
+        public SearchModel(string name, string areaType, string northeastGeopoint, string soutwestGeopoint)
+        {
+            if (name == null)
+
+            Name = name;
+            AreaType = areaType;
+            NortheastGeopoint = northeastGeopoint;
+            SouthwestGeopoint = soutwestGeopoint;
+        }
+
+        public string Name { get; set; }
+        public string AreaType { get; set; }
+        public string NortheastGeopoint { get; set; }
+        public string SouthwestGeopoint { get; set; }
+    }
+}
---
+++ SearchAreaWeb/SearchAreaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchAreaWeb.Startup))]
+namespace SearchAreaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
